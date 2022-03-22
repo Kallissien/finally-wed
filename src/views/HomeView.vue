411380e6d39454af02b0f2b1b6d3f4bf88449049
Kallissien <luke.costi@gmail.com>
2022-03-22 23:40:14 +0000

    Set up homepage columns

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,51 @@
 <script setup>
 import TheWelcome from '@/components/TheWelcome.vue'
+import WedLogo from '@/components/atoms/WedLogo.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="flex-container">
+    <section class="col-left">
+      <WedLogo class="logo"></WedLogo>
+    </section>
+    <section class="col-right">
+    </section>
   </main>
 </template>
+<style scoped>
+  .flex-container{
+    min-height: 100vh;
+    overflow: hidden;
+    display: flex;
+  }
+  .col-left, .col-right{
+    box-sizing: border-box;
+    width: 50vw;
+    height: 100vh;
+  }
+  .col-left{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background: #fff;
+    padding: 4rem
+  }
+  .col-right{
+    background-image: url('@/assets/wedding-pic-bw.png');
+    background-size: cover;
+    background-position: center;
+  }
+  .logo{
+    width: 20rem;
+    height: 20rem;
+  }
+  @media screen and (max-width: 800px){
+    .flex-container{
+      flex-direction: column;
+    }
+    .col-left, .col-right{
+      width: 100vw;
+      height: 50vh;
+    }
+  }
+</style>
