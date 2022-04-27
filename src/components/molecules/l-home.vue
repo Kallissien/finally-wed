54c7ce02a0c5f3e2cff7d4ff34047baf9033c0ba
Luke Costi <luke@usepixie.com>
2022-04-27 19:01:17 +0100

    Testing netlify form in view

--- src/components/molecules/l-home.vue
+++ src/components/molecules/l-home.vue
@@ -4,8 +4,11 @@ import LLogo from "@/components/atoms/l-logo.vue";
 <template>
   <l-logo class="logo"></l-logo>
   <div class="content">
-    <h1>Home</h1>
-    <p class="text-lg">5th August 2022</p>
-    <p class="text-lg">Marine Hotel Ballycastle</p>
+    <h1>
+      <p class="text-lg">5th August 2022</p>
+      <p class="text-lg">Marine Hotel Ballycastle</p>
+    </h1>
+    <p>Please <router-link to="/rsvp">RSVP</router-link> to let us know whether you can make it</p>
+    
   </div>
 </template>
