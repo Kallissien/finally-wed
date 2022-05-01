98b1e37909ab94a24163702743d04be1a5fe5d82
Luke <luke.costi@gmail.com>
2022-05-01 20:08:06 +0100

    Added separate menu forms for netlify

---
+++ src/components/organisms/l-menu-form.vue
@@ -0,0 +1,114 @@
+<template>
+    <form class="form col menu" name="menu-adult" method="POST" type="hidden">
+        <p class="form-header">Please fill out your menu options</p>
+
+        <div class="form-section col" v-for="course in menu" :key="course.id">
+            <p class="menu-course_header">{{ course.name }}</p>
+            <label class="input-item" v-for="data in course.options" :key="data.id" :iName="data.name">
+            <div class="menu-item">
+                <p class="menu-item_heading">{{ data.label1 }}</p>
+                <p class="menu-item_sub">{{ data.label2 }}</p>
+            </div>
+            <input :type="data.type" :name="data.name" :id="data.id" value="{{data.label1}}">
+            </label>
+        </div>
+    </form>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            menu: {
+                soup: {
+                id: 1,
+                name: "Soup",
+                options: [
+                    {
+                    id: 1.1,
+                    name: "soup",
+                    label1: "Comber potato & leek",
+                    label2: "crispy parsnips",
+                    type: "radio",
+                    },
+                ],
+                },
+                starter: {
+                id: 2,
+                name: "Starter",
+                options: [
+                    {
+                    id: 2.1,
+                    name: "starter",
+                    label1: "Smoked cheese and spring onion fishcake",
+                    label2: "with mixed leaves, sour cream and crispy dulse.",
+                    type: "radio",
+                    },
+                    {
+                    id: 2.2,
+                    name: "starter",
+                    label1: "Crispy ham hock and Gracehill black pudding croquettes",
+                    label2:
+                        "rocket leaves, homemade piccalilli and toasted sourdough.",
+                    type: "radio",
+                    },
+                    {
+                    id: 2.3,
+                    name: "starter",
+                    label1: "Vegetarian option",
+                    label2: "sourced locally on the day",
+                    type: "radio",
+                    },
+                ],
+                },
+                main: {
+                id: 3,
+                name: "Main",
+                options: [
+                    {
+                    id: 3.1,
+                    name: "main",
+                    label1: "Seared salmon fillet",
+                    label2: "smoked chorizo and prawn cream.",
+                    type: "radio",
+                    },
+                    {
+                    id: 3.2,
+                    name: "main",
+                    label1: "Roast county Antrim turkey and ham",
+                    label2: "cranberry stuffing and rich pan jus",
+                    type: "radio",
+                    },
+                    {
+                    id: 3.3,
+                    name: "main",
+                    label1: "Vegetarian option",
+                    label2: "sourced locally on the day",
+                    type: "radio",
+                    },
+                ],
+                },
+                dessert: {
+                id: 4,
+                name: "Dessert",
+                options: [
+                    {
+                    id: 4.1,
+                    name: "dessert",
+                    label1: "Sticky toffee pudding",
+                    label2: "Bushmills toffee sauce and Morelli's honeycomb ice cream",
+                    type: "radio",
+                    },
+                    {
+                    id: 4.2,
+                    name: "dessert",
+                    label1: "Spiced apple and cinnamon crumble",
+                    label2: "Morelli's vanilla ice cream",
+                    type: "radio",
+                    },
+                ],
+                },
+      }
+        }
+    }
+}
+</script>
---
+++ src/components/organisms/l-menu-kids-form.vue
@@ -0,0 +1,126 @@
+<template>
+    <form class="form col menu" name="menu-kids" method="POST" type="hidden">
+        <h3>Kids menu</h3>
+        <p class="form-header">Please fill out your menu options</p>
+
+        <div class="form-section col" v-for="course in menu" :key="course.id">
+
+            <p class="menu-course_header">{{ course.name }}</p>
+            
+            <label class="input-item" v-for="data in course.options" :key="data.id" :iName="data.name">
+                <div class="menu-item">
+                    <p class="menu-item_heading">{{ data.label1 }}</p>
+                    <p class="menu-item_sub">{{ data.label2 }}</p>
+                </div>
+
+                <input :type="data.type" :name="data.name" :id="data.id">
+                <button type="submit">I want this food, no take backsies</button>
+            </label>
+        </div>
+    </form>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            menu: {
+                starter: {
+                id: 1,
+                name: "Starter",
+                options: [
+                    {
+                        id: 1.1,
+                        name: "starter",
+                        label1: "Kids soup of the day",
+                        label2: "with wheaten bread",
+                        type: "radio",
+                    },
+                    {
+                        id: 1.2,
+                        name: "starter",
+                        label1: "Seafood chowder",
+                        label2: "with wheaten bread",
+                        type: "radio",
+                    },
+                    {
+                        id: 1.3,
+                        name: "starter",
+                        label1: "Cheesy garlic bread",
+                        label2: "with mixed leaves",
+                        type: "radio",
+                    },
+                ],
+                },
+                main: {
+                id: 2,
+                name: "Main",
+                options: [
+                    {
+                        id: 2.1,
+                        name: "main",
+                        label1: "Tempura chicken goujons",
+                        label2: "with skinny fries",
+                        type: "radio",
+                    },
+                    {
+                        id: 2.2,
+                        name: "main",
+                        label1: "Battered fish and mushy peas",
+                        label2: "with skinny fries",
+                        type: "radio",
+                    },
+                    {
+                        id: 2.3,
+                        name: "main",
+                        label1: "Cheesburger",
+                        label2: "with skinny fries",
+                        type: "radio",
+                    },
+                    {
+                        id: 2.4,
+                        name: "main",
+                        label1: "Sausages and beans",
+                        label2: "with skinny fries",
+                        type: "radio",
+                    },
+                    {
+                        id: 2.4,
+                        name: "main",
+                        label1: "Roast chicken breast",
+                        label2: "with creamy mash, broccoli and gravy",
+                        type: "radio",
+                    },
+                ],
+                },
+                dessert: {
+                id: 3,
+                name: "Dessert",
+                options: [
+                    {
+                        id: 3.1,
+                        name: "dessert",
+                        label1: "Ice cream sundae",
+                        label2: "Morelli's ice cream",
+                        type: "radio",
+                    },
+                    {
+                        id: 3.2,
+                        name: "dessert",
+                        label1: "Warm chocolate fudge cake",
+                        label2: "Morelli's ice cream",
+                        type: "radio",
+                    },
+                    {
+                        id: 3.3,
+                        name: "dessert",
+                        label1: "Fresh fruit salad",
+                        label2: "raspberry sorbet",
+                        type: "radio",
+                    },
+                ],
+                },
+            }
+        }
+    }
+}
+</script>
